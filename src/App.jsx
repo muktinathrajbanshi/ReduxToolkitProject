b8c292a848759b8ee05365d390632424c6432425
unknown <muktinathrajbanshi786@gmail.com>
2025-12-04 06:37:11 +0200

    new logic is build

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,22 @@ const App = () => {
       <div className="currency-div">
         <h1>Currency Converter</h1>
         <hr />
-
+        <div>
+          <label>
+            Amount:
+            <input type="text" />
+          </label>
+        </div>
+        <section className="currency-selector">
+          <label>
+          From:
+          <select>
+          {["USD", "EUR", "NPR", "GBP", "AUD"].map((currency) => {
+            return <option key={currency} value="USD">USD</option>;
+          })}
+          </select>
+          </label>
+        </section>
       </div>
     </section>
   )
